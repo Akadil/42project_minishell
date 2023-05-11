4ad9fe384e4363d7bba5ead3d0bd478c2164b770
akadil.kalimoldayev@gmail.com <akadil.kalimoldayev@gmail.com>
2023-05-11 19:06:41 +0200

    Finished and tested assign types for parsing

--- srcs/parsing/ft_assign_types.c
+++ srcs/parsing/ft_assign_types.c
@@ -6,7 +6,7 @@
 /*   By: akalimol <akalimol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 15:28:25 by akalimol          #+#    #+#             */
-/*   Updated: 2023/05/11 16:08:12 by akalimol         ###   ########.fr       */
+/*   Updated: 2023/05/11 16:21:30 by akalimol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,23 +40,23 @@ void    ft_assign_types(t_list *node)
     {
         if (is_filename == 1 && is_filename--)
             node->type = FILENAME;
-        if (ft_strcmp(node->content, "|"))
+        else if (!ft_strcmp(node->content, "|"))
             node->type = PIPE;
-        else if(ft_strcmp(node->content, "||"))
+        else if(!ft_strcmp(node->content, "||"))
             node->type = OR;
-        else if (ft_strcmp(node->content, "&&"))
+        else if (!ft_strcmp(node->content, "&&"))
             node->type = AND;
-        else if (ft_strcmp(node->content, "("))
+        else if (!ft_strcmp(node->content, "("))
             node->type = LEFT_P;
-        else if (ft_strcmp(node->content, ")"))
+        else if (!ft_strcmp(node->content, ")"))
             node->type = RIGHT_P;
-        else if (ft_strcmp(node->content, "<") && is_filename++)
+        else if (!ft_strcmp(node->content, "<") && is_filename++)
             node->type = REDIRECT_IN;
-        else if (ft_strcmp(node->content, "<<") && is_filename++)
+        else if (!ft_strcmp(node->content, "<<") && is_filename++)
             node->type = HEREDOC;
-        else if (ft_strcmp(node->content, ">") && is_filename++)
+        else if (!ft_strcmp(node->content, ">") && is_filename++)
             node->type = REDIRECT_OUT;
-        else if (ft_strcmp(node->content, ">>") && is_filename++)
+        else if (!ft_strcmp(node->content, ">>") && is_filename++)
             node->type = REDIRECT_OUT2;
         else 
             node->type = PARAMETER;
---
+++ tests/test_assgn_types.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_assgn_types.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akalimol <akalimol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 16:13:46 by akalimol          #+#    #+#             */
+/*   Updated: 2023/05/11 16:23:10 by akalimol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include "linked_list.h"
+
+/*
+    tester: 
+    cc -g -I includes -Wall -Wextra -Werror tests/test_assgn_types.c srcs/parsing/ft_add_spaces.c srcs/parsing/ft_tokenization.c srcs/parsing/ft_assign_types.c -lreadline -Llibft -lft; ./a.out; rm a.out
+
+*/
+
+t_list    *ft_tokenization(char *str);
+void    print_tokens(t_list *token);
+char    *ft_add_spaces(char *str);
+void    ft_assign_types(t_list *node);
+
+int main(void)
+{
+    char    *str;
+    t_list  *head;
+
+    head = NULL;
+    str = readline("Type the string: ");
+    while (str)
+    {
+        write(1, &"--------------------------------------\n", 40);
+        write(1, &"\n", 1);
+        
+        str = ft_add_spaces(str);
+
+        head = ft_tokenization(str);
+        ft_assign_types(head);
+        print_tokens(head);
+        
+        write(1, &"\n", 1);
+        str = readline("Type the string: ");
+        
+    }
+
+    return (0);
+}
+
+void    print_tokens(t_list *token)
+{
+    while (token)
+    {
+        printf("%s -> %d\n", (char *)token->content, token->type);
+        token = token->next;
+    }
+}

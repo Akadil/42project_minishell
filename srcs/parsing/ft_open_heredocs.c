b5851d3396faec8a98541cff1ed8ab0ebf1c30ba
akadil.kalimoldayev@gmail.com <akadil.kalimoldayev@gmail.com>
2023-05-24 17:19:18 +0200

    Finished and tested open heredocs

--- srcs/parsing/ft_open_heredocs.c
+++ srcs/parsing/ft_open_heredocs.c
@@ -6,7 +6,7 @@
 /*   By: akalimol <akalimol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 03:32:32 by akalimol          #+#    #+#             */
-/*   Updated: 2023/05/24 05:36:25 by akalimol         ###   ########.fr       */
+/*   Updated: 2023/05/24 17:10:28 by akalimol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 #include <unistd.h>
 #include <readline/readline.h>
 
-int	ft_strcmp_alt(const char *s1, const char *s2);
 int    ft_get_heredoc(char *delimiter);
 
 int ft_open_heredocs(t_list *head)
@@ -59,7 +58,7 @@ int    ft_get_heredoc(char *delimiter)
         return (-1);
     }
 	line = readline(">");
-	while (line && ft_strcmp_alt(line, delimiter) != 0)
+	while (line && ft_strcmp(line, delimiter) != 0)
 	{
 		ft_putstr_fd(line, fd[0]);
 		free(line);
@@ -68,18 +67,4 @@ int    ft_get_heredoc(char *delimiter)
     free(line);
     close (fd[0]);
     return (fd[1]);
-}
-
-int	ft_strcmp_alt(const char *s1, const char *s2)
-{
-	size_t	i;
-
-	i = 0;
-	while ((s1[i] || s2[i]))
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
-	}
-    return (s1[i] - s2[i]);
 }
--- tests/test_open_heredoc.c
+++ tests/test_open_heredoc.c
@@ -6,13 +6,21 @@
 /*   By: akalimol <akalimol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 05:14:50 by akalimol          #+#    #+#             */
-/*   Updated: 2023/05/24 05:19:17 by akalimol         ###   ########.fr       */
+/*   Updated: 2023/05/24 17:18:53 by akalimol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "struct_list.h"
 #include "libft.h"
 #include <stdio.h>
+
+/*
+    Test:
+        cc -g3 -I includes -Wall -Werror -Wextra tests/test_open_heredoc.c srcs/parsing/ft_open_heredocs.c srcs/utils/ft_clean.c srcs/utils/ft_error_1.c srcs/utils/ft_error_2.c  -Llibft -lft -lreadline
+
+    Result:
+        1. Perfectly working. But not integrated the ft_expand of the string
+*/
 
 int ft_open_heredocs(t_list *head);
 
@@ -21,8 +29,8 @@ int main(void)
     t_list  *head;
 
     head = NULL;
-    ft_lstadd_back(&head, ft_lstnew("EOF", 8));
-    ft_lstadd_back(&head, ft_lstnew("Finir", 8));
+    ft_lstadd_back(&head, ft_lstnew(ft_strdup("EOF"), 8));
+    ft_lstadd_back(&head, ft_lstnew(ft_strdup("Finir"), 8));
 
     ft_open_heredocs(head);
 
